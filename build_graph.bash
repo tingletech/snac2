b9e5a64afd7b53530d4096c032420161d56a4888
Brian Tingle <brian.tingle@ucop.edu>
2012-07-26 10:16:49 -0400

    new wrapper script for running gremlin to build database from EAC

---
+++ build_graph.bash
@@ -0,0 +1,17 @@
+#!/bin/env bash
+# this script processes EAC, creating a neo4j db
+set -eux
+# need these vars defined
+echo $GRAPH_DB 
+echo $EAC_DIR
+echo GRAPH_ML
+read -p "answer y to blow away the old index? " -n 1
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    exit 1
+fi
+rm -r $GRAPH_DB
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" # http://stackoverflow.com/questions/59895
+export JAVA_OPTIONS="-Xms32m -Xmx5g"
+# create new database
+$DIR/gremlin.sh -e $DIR/load/load_eac.grm
