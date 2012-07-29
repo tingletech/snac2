0aae7f2226c30664bb34010fccb1d18d7b6527bb
Brian Tingle <brian.tingle@ucop.edu>
2012-07-28 21:05:22 -0400

    grab rexster from the web rather than ssh

--- pull_rexster.bash
+++ pull_rexster.bash
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 set -eu
 which mvn git
-git clone git@github.com:tingletech/rexster.git
+git clone https://github.com/tingletech/rexster.git
 cd rexster
+mvn install
+cd ..
+cd rexster-extension
 mvn install
 cd ..
