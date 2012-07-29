42ddffc47c1df37b6c29c3c4597b167d7652c276
Brian Tingle <brian.tingle@ucop.edu>
2012-07-28 21:07:34 -0400

    put all the python libraries in this script

--- install_python_libraries.bash
+++ install_python_libraries.bash
@@ -3,3 +3,4 @@ set -eu
 export PYTHONPATH=~/python-packages/lib/python2.6/site-packages
 mkdir -p $PYTHONPATH
 easy_install --prefix ~/python-packages rdflib
+easy_install --prefix ~/python-packages twill
