12ff63c917f705a5ee5d2ae20a9e7878d98e5327
Brian Tingle <brian.tingle.cdlib.org@gmail.com>
2012-07-29 23:48:08 -0400

    add pythonpath/bin to PATH

--- setenv.bash
+++ setenv.bash
@@ -1,4 +1,5 @@
 #!/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" # http://stackoverflow.com/questions/59895
 export PYTHONPATH=~/python-packages/lib/python2.6/site-packages
+export PATH=~/python-packages/bin:$PATH
 alias monit="monit -c $DIR/servers/monitrc"
