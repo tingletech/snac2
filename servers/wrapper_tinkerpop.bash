0dc22cb07b82af1c5ad67a50f9f4de36c40e4d0a
Brian Tingle <brian.tingle.cdlib.org@gmail.com>
2012-07-31 19:06:35 -0400

    cd to webapp directory to be able to find files

--- servers/wrapper_tinkerpop.bash
+++ servers/wrapper_tinkerpop.bash
@@ -2,5 +2,6 @@
 set -ue
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" # http://stackoverflow.com/questions/59895
 export CATALINA_BASE=$DIR/tomcat_tinkerpop
+cd $CATALINA_BASE/webapps/rexster/
 # http://stackoverflow.com/questions/3356476/debugging-monit
 $@
