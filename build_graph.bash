f70a0121fcea31c19d83d24a517d3abc5a687306
SNAC user <snac2@shannon.Village.Virginia.EDU>
2014-08-18 15:30:20 -0400

    tweak shell script

--- build_graph.bash
+++ build_graph.bash
@@ -4,7 +4,7 @@ set -eux
 # need these vars defined
 echo $GRAPH_DB 
 echo $EAC_DIR
-echo GRAPH_ML
+echo $GRAPH_ML
 if [[ -e $GRAPH_DB ]]; then
   read -p "answer y to blow away the old index? " -n 1
   if [[ ! $REPLY =~ ^[Yy]$ ]]
@@ -16,4 +16,4 @@ fi
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" # http://stackoverflow.com/questions/59895
 export JAVA_OPTIONS="-Xms32m -Xmx5g"
 # create new database
-$DIR/gremlin.sh -e $DIR/load/load_eac.grm
+$DIR/gremlin/bin/gremlin.sh -e $DIR/load/load_eac.grm
